40d5cd026b64df457a8bfb61c35e6ce55badc67b
Derek Slone-Zhen <derek@slone-zhen.com>
2023-01-29 11:13:38 +1100

    Multiple, fully-independant pianos

--- js/piano-keyboard.js
+++ js/piano-keyboard.js
@@ -32,10 +32,41 @@ function* build_keys(from, to) {
   throw new RangeError(`to=${to} is not a valid key`)
 }
 
+// `piano_ish` can be a piano element, a sibling or close-cousin, of an id
+// This allows portable onclick actions that simply use `this` as a parameter
+function piano_from_piano_ish(piano_ish) {
+  if (piano_ish instanceof String) {
+    piano = document.getElementById(piano_ish)
+  } else if (piano_ish instanceof HTMLElement) {
+    if ("piano-keyboard" in piano_ish.classList) {
+      piano = piano_ish
+    } else {
+      for(;;) {
+        piano = piano_ish.getElementsByClassName("piano-keyboard")
+        if (piano.length == 1) {
+          piano = piano[0]
+          break
+        }
+        if (piano.length > 1) {
+          throw Error("Too many pianos found")
+        }
+        piano_ish = piano_ish.parentElement
+        if (piano_ish === null) {
+          throw Error("No piano found")
+        }
+      }
+    }
+  }
+  return piano
+}
+
 // Main piano keyboard builder
-function buildKeyBoard(piano) {
+function buildKeyBoard(piano_ish) {
+
+  piano = piano_from_piano_ish(piano_ish)
   piano.innerHTML = ""
 
+  // Datasets, see: https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
   let dataset = piano.dataset
   let from = dataset?.firstKey || DEFAULTS[0]
   let to = dataset?.lastKey || DEFAULTS[1]
@@ -96,7 +127,8 @@ window.addEventListener("load", initPianos, true)
 
 // Manipulators
 
-function add_dots(piano, key_spec) {
+function add_dots(piano_ish, key_spec) {
+  piano = piano_from_piano_ish(piano_ish)
   for (key of key_spec.replace("#", "♯").replace("b", "♭").split(" ")) {
     parts = key.split(":")
     key = parts[0]
@@ -116,27 +148,25 @@ function add_dots(piano, key_spec) {
   }
 }
 
-function reset(root = document) {
-  elmts = root.getElementsByClassName("key-pad")
-  for (i in elmts) {
-    e = elmts[i]
+function reset(piano_ish) {
+  piano = piano_from_piano_ish(piano_ish)
+  for (let e of piano.getElementsByClassName("key-pad")) {
     e.innerHTML = "&nbsp;"
     e.style = 'transparent';
   }
 }
 
-function change_key_text_colour(key_spec) {
-  KEYS = key_spec.split(" ")
-  for (i in KEYS) {
-    key = KEYS[i]
-    console.trace(key)
-    console.trace("\n")
-    elm = document.getElementById(key);
+function change_key_text_colour(piano_ish, key_spec) {
+  piano = piano_from_piano_ish(piano_ish)
+  for (let key_name of key_spec.split(" ")) {
+    // console.trace(key_name)
+    // console.trace("\n")
+    let key_elmt = piano.getElementById(piano_ish + '-' + key_name);
     color = "red"
     // if (elm.classList.contains("key-sharp")) {
     //   color = "yellow"
     // }
-    elm.style.color = color
+    key_elmt.style.color = color
     // ⬤
   }
 }
